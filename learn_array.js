ce75298b25560876e74b6615bb77324b60c8364a
Rokeya272 <rokeyafabby60@gmail.com>
2022-09-03 01:30:49 +0600

    rename

---
+++ learn_array.js
@@ -0,0 +1,20 @@
+
+//Array modify
+let list = ['orange','cheese','milk'];
+
+list[1]='tomato';
+list[list.length]='potato';
+list[list.length]='ice cream';
+console.log(list);
+
+//push method
+let list1 = ['orange','cheese','milk'];
+
+list1.push('mango');
+console.log(list1);
+
+//pop method
+let list2 = ['orange','cheese','milk'];
+
+list2.pop()
+console.log(list2);
