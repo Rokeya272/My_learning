dc221b55ba90e052a7e4d0046ff48aab8675fb1f
Rokeya272 <rokeyafabby60@gmail.com>
2022-09-03 01:47:52 +0600

    push,pop,shift,unshift

--- learn_array.js
+++ learn_array.js
@@ -7,14 +7,29 @@ list[list.length]='potato';
 list[list.length]='ice cream';
 console.log(list);
 
-//push method
+//push method[at end]
 let list1 = ['orange','cheese','milk'];
 
 list1.push('mango');
 console.log(list1);
 
-//pop method
+//pop method[at end]
 let list2 = ['orange','cheese','milk'];
 
 list2.pop()
 console.log(list2);
+
+//unshift(at begin like push)
+let list3 = ['orange','cheese','milk'];
+
+list3.unshift('lemon');
+console.log(list3);
+
+
+//shift(at start like pop)
+
+let list4 = ['orange','cheese','milk'];
+list4.shift();
+
+
+console.log(list4);
